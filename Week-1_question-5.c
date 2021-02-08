f3a7e09a800c7591ced28098aec57cd1802d87f1
Komal Behera <78549831+komal53@users.noreply.github.com>
2021-02-08 15:48:08 +0530

    Add files via upload

--- Week-1_question-5.c
+++ Week-1_question-5.c
@@ -23,6 +23,21 @@ int main()
         case 'U':
             printf("Vowel");
             break;
+        case 'a':
+            printf("Vowel");
+            break;
+        case 'e':
+            printf("Vowel");
+            break;
+        case 'i':
+            printf("Vowel");
+            break;
+        case 'o':
+            printf("Vowel");
+            break;
+        case 'u':
+            printf("Vowel");
+            break;
         default:
             printf("Consonant");
     }
